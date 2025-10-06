c94d185b8efd1fc1bda878f3137376625ddc7143
Shimanto55 <dicshimu608@gmail.com>
2025-10-06 14:08:24 +0600

    Added SEO meta tags, sitemap, and robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Md.MashrulIslam/", // ⚠️ your repo name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
